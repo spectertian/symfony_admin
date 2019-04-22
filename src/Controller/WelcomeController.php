c2ca9540f1e900e6b28b485188f853100e722942
tianzhongsheng <tianzhongsheng@lingxi360.com>
2019-04-22 22:09:15 +0800

    def

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -13,7 +13,7 @@ class WelcomeController extends AbstractController
     public function index()
     {
         return $this->render('welcome/index.html.twig', [
-            'controller_name' => 'WelcomeController',
+            'controller_name' => 'WelcomeController--11',
         ]);
     }
 }
